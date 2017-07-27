e412df423d141d6516ef3253a9622ad38c539e8f
Peter Pham <ptr.pham@gmail.com>
2017-07-27 07:42:09 -0700

    More correct version of more interpretable version.

--- glsl/po1.fs
+++ glsl/po1.fs
@@ -7,39 +7,45 @@ uniform vec3 start, end;
 uniform float radius;
 
 void main() {
+  vec3 normal = normalize(v_normal);
   vec3 d0 = start - v_position;
   vec3 d1 = end - v_position;
-  vec3 diff = end - start;
+  vec3 dir = end - start;
 
   float l0 = length(d0);
   float l1 = length(d1);
-  float ld = length(diff);
+  float ld = length(dir);
 
   d0 /= l0;
   d1 /= l1;
-  diff /= ld;
+  dir /= ld;
 
-  float t = clamp(dot(v_position - start, diff), 0.0, ld);
-  vec3 approach = t*diff + start - v_position;
+  float t = clamp(dot(v_position - start, dir), 0.0, ld);
+  vec3 approach = t*dir + start - v_position;
   float la = length(approach);
   approach = approach / la;
 
-  float theta0 = acos(clamp(dot(d0, approach), -1.0, 1.0));
-  float theta1 = acos(clamp(dot(d1, approach), -1.0, 1.0));
+  float gammaa = asin(min(radius/la, 1.0));
+  float gamma0 = asin(min(radius/l0, 1.0));
+  float gamma1 = asin(min(radius/l1, 1.0));
 
-  float radius2 = radius*radius;
-  float hyp0 = sqrt(radius2 + l0*l0);
-  float hyp1 = sqrt(radius2 + l1*l1);
-  float hypa = sqrt(radius2 + la*la);
+  float theta0_raw = acos(clamp(dot(d0, approach), -1.0, 1.0));
+  float theta1_raw = acos(clamp(dot(d1, approach), -1.0, 1.0));
 
-  float st0 = 1.0 - l0 / hyp0;
-  float st1 = 1.0 - l1 / hyp1;
-  float sta = 1.0 - la / hypa;
+  float theta0 = max(theta0_raw - 0.5*gammaa, 0.0);
+  float theta1 = max(theta1_raw - 0.5*gammaa, 0.0);
 
-  float ndot = max(dot(v_normal, approach), 0.0);
+  float ndot0 = dot(normal, d0);
+  float ndot1 = dot(normal, d1);
+  float ndota = dot(normal, approach);
+
+  float st0 = 0.5*(ndot0 + 1.0) * (1.0 - cos(gamma0));
+  float st1 = 0.5*(ndot1 + 1.0) * (1.0 - cos(gamma1));
+  float sta = 0.5*(ndota + 1.0) * (1.0 - cos(gammaa));
 
   float factor = sta + 0.5*theta0*(sta + st0) + 0.5*theta1*(sta + st1);
-  float value = 1.0 - ndot*factor;
+  float value = 1.0 - factor;
+
   gl_FragColor = vec4(value, value, value, 1.0);
 }
 
